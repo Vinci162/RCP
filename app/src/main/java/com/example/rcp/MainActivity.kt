b1787cee9a5007ac4632cb01936f7f22f5ae7b93
Vinci162 <145670103+Vinci162@users.noreply.github.com>
2024-05-02 17:44:35 +0200

    Update MainActivity.kt
    ide stop button + tokeny

--- app/src/main/java/com/example/rcp/MainActivity.kt
+++ app/src/main/java/com/example/rcp/MainActivity.kt
@@ -13,18 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val stopToken = 1
+        val startToken = 0
+
         val startButton = findViewById<Button>(R.id.startbutton)
         startButton.setOnClickListener {
-            makeHttpRequest()
+            makeHttpRequestStart(startToken)
+        }
+
+        val stopButton = findViewById<Button>(R.id.stopbutton)
+        stopButton.setOnClickListener {
+            makeHttpRequestStop(stopToken)
         }
     }
 
-    private fun makeHttpRequest() {
+    private fun makeHttpRequestStart(startToken: Int) {
         val client = OkHttpClient()
 
         val url = "http://10.0.2.2:8080/"
+        val requestBody = FormBody.Builder()
+            .add("stop_token", startToken.toString())
+            .build()
+
         val request = Request.Builder()
             .url(url)
+            .post(requestBody)
             .build()
 
         client.newCall(request).enqueue(object : Callback {
@@ -46,4 +59,38 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun makeHttpRequestStop(stopToken: Int) {
+        val client = OkHttpClient()
+
+        val url = "http://10.0.2.2:8080/"
+        val requestBody = FormBody.Builder()
+            .add("stop_token", stopToken.toString())
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Request failed: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (!response.isSuccessful) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Request failed: ${response.code}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                val responseBody = response.body?.string()
+            }
+        })
+    }
+
 }
